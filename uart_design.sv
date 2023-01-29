6e503c6dcd83b1bd0ae477d2664512ba30b2797e
Antonis Keremidis <39350193+toniker@users.noreply.github.com>
2023-01-29 18:57:01 +0200

    Update uart_design.sv

--- uart_design.sv
+++ uart_design.sv
@@ -67,7 +67,7 @@ reg mes_end = 0 ;
 baud_controller baud_controller_tx_instance (give_reset, give_clk, baud_select, Tx_sample_ENABLE);
 
 reg [2:0] current_state, next_state;
-reg [2:0] IDLE = 3'b000, TSTART = 3'b001, TDATA = 3'b010, TPARITY = 3'b011, TSTOP = 3'b100;
+  reg [2:0] IDLE = 3'b000, TSTART = 3'b001, TDATA = 3'b010, TPARITY = 3'b011, TSTOP = 3'b100;
 
 always @(posedge Tx_sample_ENABLE or posedge give_reset)
 begin
@@ -83,7 +83,6 @@ end
   
   always@(posedge mes_end)
     begin
-      mes_end = 0;
       next_state = TPARITY;
     end
   
@@ -148,28 +147,35 @@ begin
           end
 
         TDATA:
+          begin
           if(counter == 0)
             begin
-                TxD = Tx_DATA[bitIndex];
-              if (bitIndex == 7)
+              TxD = Tx_DATA[bitIndex];
+              if (bitIndex == 8)
                 begin
                 bitIndex = 0;
                 mes_end = 1;
                 end
               else
                 begin
-                bitIndex = bitIndex + 1;
+//                 bitIndex = bitIndex + 1;
             	end
            end
+            if(counter == 15)
+        	begin
+          		bitIndex = bitIndex + 1;
+        	end
+          end
 
         TPARITY:
-//           if(counter == 1)
+          //if(counter == 1)
             begin
+              mes_end = 0;
                 TxD = ^Tx_DATA;
             end
 
         TSTOP:
-//           if(counter == 1)
+           //if(counter == 1)
             begin
               TxD = 1;
               TX_BUSY = 0;
@@ -197,21 +203,22 @@ input give_clk, give_reset;
 input [2:0] baud_select;
 input RX_EN;
   
-output RxD;
-output [7:0] Rx_DATA;
-output Rx_FERROR; // Framing Error //
-output Rx_PERROR; // Parity Error //
-output Rx_VALID; // Rx_DATA is Valid //
+input reg RxD;
+output reg[7:0] Rx_DATA;
+output reg Rx_FERROR; // Framing Error //
+output reg Rx_PERROR; // Parity Error //
+output reg Rx_VALID; // Rx_DATA is Valid //
 
 reg [3:0] counter;
-reg [3:0] bitIndex = 0;
+reg [3:0] bitIndex = 7;
 wire Rx_sample_ENABLE;
 reg mes_end = 0 ;
+reg expected_parity = 0;
   
 baud_controller baud_controller_rx_instance(give_reset, give_clk, baud_select, Rx_sample_ENABLE);
 
 reg [2:0] current_state, next_state;
-  reg [2:0] IDLE = 3'b000, TSTART = 3'b001, TDATA=3'b010 , TVALIDE=3'b011, TSTOP=3'b100;
+  reg [2:0] IDLE = 3'b000, TSTART = 3'b001, TDATA=3'b010 , TVALIDATE=3'b011, TSTOP=3'b100;
   
   always @(posedge Rx_sample_ENABLE or posedge give_reset)
 begin
@@ -235,7 +242,7 @@ end
 begin 
     case(current_state)
         IDLE:
-          if(RX_EN==1)
+          if(RX_EN==1 && RxD == 0)
             begin
                 next_state = TSTART;
             end
@@ -251,7 +258,7 @@ begin
             end
           
         TDATA:
-          if(counter == 15 && bitIndex < 7) 
+          if(counter == 15 && bitIndex > 0) 
             begin
                 next_state = TDATA;
             end
@@ -288,33 +295,63 @@ begin
       
   		 TSTART:
           begin
-              RxD = 0;
-              bitIndex = 0;
+            bitIndex = 7;
           end
       
     	 TDATA:
-          if(counter == 0)
+           if(counter == 7)
             begin
-                RxD = Rx_DATA[bitIndex];
-              if (bitIndex == 7)
+              Rx_DATA[bitIndex] <= RxD;
+              if (bitIndex == 0)
                 begin
-                bitIndex = 0;
+                bitIndex = 7;
                 mes_end = 1;
                 end
               else
                 begin
-                bitIndex = bitIndex + 1;
+                bitIndex = bitIndex - 1;
             	end
            end
       
       	TVALIDATE:
           begin
-              
+            expected_parity = ^Rx_DATA;
+            if(expected_parity != RxD)
+              begin
+                Rx_PERROR = 1;
+              end
             end
       
       	TSTOP:
             begin
+              if(Rx_DATA != 1)
+                begin
+                  Rx_FERROR = 1;
+                end
               
+              if(Rx_FERROR || Rx_PERROR)
+                begin
+                  Rx_VALID = 0;
+                end
+              else
+                begin
+                  Rx_VALID = 1;
+                end
             end
-      
+ 
+    endcase
+end
+  
+always@(posedge Rx_sample_ENABLE or posedge give_reset)
+begin
+    if(give_reset || counter == 15)
+    begin
+        counter = 0;
+    end
+    else
+    begin
+        counter = counter + 1;
+    end
+end
+
 endmodule
